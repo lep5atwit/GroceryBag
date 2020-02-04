2659ee186f6fb5e6560c25d65dfa44f80cdb2936
Devin Salter <salterd@wit.edu>
2020-02-04 14:46:57 -0500

    GroceryBagger.java stylized

--- src/edu/wit/dcsn/comp2000/bagapp/GroceryBagger.java
+++ src/edu/wit/dcsn/comp2000/bagapp/GroceryBagger.java
@@ -7,28 +7,34 @@ import edu.wit.dcsn.comp2000.bagadt.ResizableArrayBag;
 
 public class GroceryBagger {
 
-	public static void main(String args[]) {
+	public static void main(String args[])
+	{
 		
 		// Creates items from groceries.txt
-		Scanner sc = new Scanner(System.in); //replace with groceries.txt
-		ArrayList<String> items = new ArrayList<String>();
-		while(sc.hasNext()==true) {
-			items.add(sc.nextLine());
-		}
+		Scanner sc = new Scanner( System.in ) ; //replace with groceries.txt
+		ArrayList<String> items = new ArrayList<String>() ;
+		while( sc.hasNext() )
+			{
+			items.add( sc.nextLine() ) ;
+			
+			}
 		
 		// Creates GroceryItems from the items
-		ArrayList<GroceryItem> groceries= new ArrayList<GroceryItem>();
-		for(int i =0; i< items.size(); i++) {
-			groceries.add(new GroceryItem(items.get(i))); 
-		}	
-	
+		ArrayList<GroceryItem> groceries = new ArrayList<GroceryItem>() ;
+		for(int index = 0 ; index < items.size() ; index++ )
+			{
+			groceries.add( new GroceryItem( items.get( index ) ) ) ; 
+			
+			}	
 		// Creates a bag 
-		ResizableArrayBag<GroceryItem> bag = new ResizableArrayBag<GroceryItem>();
+		ResizableArrayBag<GroceryItem> bag = new ResizableArrayBag<GroceryItem>() ;
 		
 		//Take GroceryItems and sorts them into bags
 		//All nonbreakables in one bag. remove from groceries
 		// parse the items into new bags
-		GroceryBag bag = new GroceryBag();
+		GroceryBag bags = new GroceryBag() ;
+		
+		
 		
 	}
 }
