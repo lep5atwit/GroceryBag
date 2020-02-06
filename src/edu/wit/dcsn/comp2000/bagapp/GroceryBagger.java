4630c4d181fc036b38e691be9f61ffda8f7e2833
lep5atwit <54985649+lep5atwit@users.noreply.github.com>
2020-02-06 10:01:34 -0500

    Implemented GroceryBagger

--- src/edu/wit/dcsn/comp2000/bagapp/GroceryBagger.java
+++ src/edu/wit/dcsn/comp2000/bagapp/GroceryBagger.java
@@ -1,5 +1,7 @@
 package edu.wit.dcsn.comp2000.bagapp;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -7,28 +9,111 @@ import edu.wit.dcsn.comp2000.bagadt.ResizableArrayBag;
 
 public class GroceryBagger {
 
-	public static void main(String args[])
-	{
-		
+	public static void main(String args[]) throws FileNotFoundException {
+
 		// Creates items from groceries.txt
-		Scanner sc = new Scanner( System.in ) ; //replace with groceries.txt
-		ArrayList<GroceryItem> groceries = new ArrayList<GroceryItem>() ;
-		while( sc.hasNext() )
-			{
-			groceries.add( new GroceryItem ( sc.nextLine().split( "\t" ) ) ) ;
-			
-			}		
-		//Take GroceryItems and sorts them into bags
-		//All nonbreakables in one bag. remove from groceries
-		// parse the items into new bags
-		ArrayList<ResizableArrayBag<GroceryItem>> bagArray = new ArrayList<>() ;
-		while( !groceries.isEmpty() )
-			{
-			
-			
-			}	
+		Scanner sc = new Scanner(new File("./data/groceries.txt")); 
+		ArrayList<GroceryItem> groceries = new ArrayList<GroceryItem>();
+		while (sc.hasNext()) {
+			groceries.add(new GroceryItem(sc.nextLine().split("\t")));
+
+		}
+		sc.close();
+		//sorts bags
+		ArrayList<ResizableArrayBag<GroceryItem>> bagArray = new ArrayList<ResizableArrayBag<GroceryItem>>();
+		bagArray = bagGroceries(groceries, bagArray);
 		
+		ResizableArrayBag<GroceryItem> currentBag;
+		GroceryItem item;
 		
-		
+				String breakable;
+				String weight;
+				String firmness;
+		//prints bags
+			
+		for(int x=0; x < bagArray.size(); x++) { //traverse array of bags
+			System.out.printf("Bag %d:%n", x+1);
+			currentBag= bagArray.get(x);
+			for(int y = currentBag.getCurrentSize(); y>0 ; y--) { //traverse the contents of each bag 
+				if(currentBag.getCurrentSize()!=0) {
+					item = currentBag.remove();
+					//breakable
+					if(item.getBreakable()==true) {
+						 breakable = "breakable";
+					} else {
+						breakable= "unbreakable";
+					}
+					//weight
+					if(item.getWeight()==1) {
+						weight = "light";
+					} else if(item.getWeight()==2) {
+						weight = "medium";
+					} else {
+						weight = "heavy";
+					}
+					//firmness
+					if(item.getFirmness()==1) {
+						firmness = "soft";
+					} else if(item.getFirmness()==2) {
+						firmness = "firm";
+					} else {
+						firmness = "hard";
+					}
+					System.out.printf("	%s(%s):	%s,	%s,	%s%n", item.getItemName(), item.getSize(), breakable, weight, firmness);
+				}
+			}
+			
+		}
 	}
+
+	/** 
+	 * Method to sort a GroceryItem array-list into Bags
+	 * @param groceries
+	 * @param bagArray
+	 * @return bagArray array of sorted bags
+	 */
+	public static ArrayList<ResizableArrayBag<GroceryItem>> bagGroceries(ArrayList<GroceryItem> groceries, ArrayList<ResizableArrayBag<GroceryItem>> bagArray) {
+		//Initialize variables
+		ResizableArrayBag<GroceryItem> bag = new ResizableArrayBag<GroceryItem>();
+		String s = "item large heavy hard rigid unbreakable unperishable";
+		String[] sArr= s.split(" ");
+		GroceryItem previousItem = new GroceryItem(sArr); // TODO: Initialize to an unbreakable at the lowest tier
+		int size=0; 
+		// Traverse the groceries, placing them into bags
+		for (int i = 0; i < groceries.size(); i++) {
+			
+			if (previousItem.compareTo(groceries.get(i)) != 1) {
+				
+				// Placing a item into a full bag...
+				if (size+groceries.get(i).getSize()>25) {  //Add current size to grocery size > 25n
+					bagArray.add(bag);
+					bag= new ResizableArrayBag<GroceryItem>();
+					size =0;
+					bag.add(groceries.get(i));
+					size= groceries.get(i).getSize()+size; 
+				}
+				
+				// Placing a item into a bag...
+				if (size+groceries.get(i).getSize()<=25) {
+					bag.add(groceries.get(i)); // add the groceryItem into the bag
+					size=size+groceries.get(i).getSize(); //		
+				}
+				
+			} 
+			
+			else { //Unable to place the item in this bag 
+				bagArray.add(bag); // Get a new bag for it
+				bag=new ResizableArrayBag<GroceryItem>();
+				size=0;
+				bag.add(groceries.get(i));
+				size=size+ groceries.get(i).getSize();
+
+			}
+
+		}
+
+		return bagArray;
+
+	}
+
 }
